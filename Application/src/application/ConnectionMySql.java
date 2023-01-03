4ccc624fb4412fdeb5ea9d98ad4a4c70cd5553af
Theo <souchontpro@gmail.com>
2023-01-03 21:31:02 +0100

    modif script green

--- Application/src/application/ConnectionMySql.java
+++ Application/src/application/ConnectionMySql.java
@@ -1,20 +1,23 @@
 package application;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionMySql {
 
-	
-	public Connection cn=null;
-	public static Connection connexionDB(){
+	public Connection cn = null;
+
+	public static Connection connexionDB() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			java.sql.Connection cn= DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root","");
-	System.out.println("Connection ok");
+			java.sql.Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "");
+			System.out.println("Connection ok");
 			return cn;
-		}catch(ClassNotFoundException | SQLException e) {
+		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("connection failed");
 			return null;
 		}
-	
-		}
+
 	}
+}
